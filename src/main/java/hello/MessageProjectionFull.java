c636b508d37208d766b0a76724b42ce50c0f9b46
tflaherty <twf@apidev.com>
2016-08-26 16:42:19 -0400

    - Added MessageProjectionFull.java.  It still isn't working correctly.

---
+++ src/main/java/hello/MessageProjectionFull.java
@@ -0,0 +1,34 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.util.Date;
+
+/**
+ * Created by Tom on 8/26/2016.
+ */
+@Projection(name = "full", types = {Message.class})
+public interface MessageProjectionFull
+{
+    @Value("#{target.getId()}")
+    public long getId();
+
+    @Value("#{target.getMessage()}")
+    public String getMessage();
+
+    @Value("#{target.getDateReceived()}")
+    public Date getDateReceived();
+
+    @Value("#{target.getRead()}")
+    public boolean isRead();
+
+    @Value("#{target.getMetadata()}")
+    public String getMetadata();
+
+    @Value("#{target.getLoyalty().getLoyaltyCode()}")
+    public String getLoyaltyCode();
+
+    @Value("#{target.getLoyalty().getId()}")
+    public long getLoyaltyId();
+}
